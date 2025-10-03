6ddfa31e8bce1ffb939fe03fa4a4bb8d85f4d69a
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-10-03 10:42:32 -0600

    Termino de darle estilo a la aplicacio, guardo e ire creando mi readMe personal para posteriormente agregarlo en el ReadmeProfesional

--- src/components/weatherMainInfo.tsx
+++ src/components/weatherMainInfo.tsx
@@ -9,12 +9,10 @@ export default function WeatherMainInfo({weather}: WeatherMainInfoProps){
         <>
             <div className="weather-main-info">
                 <div className="weather-location">
-                    <h2>{weather.location.name}</h2>
-                    <p>{weather.location.region}</p>
+                    <h2 className="weather-location-name">{weather.location.name}</h2>
+                    <p className="weather-region-text">{weather.location.region}</p>
+                    <p className="weather-country-text">{weather.location.country}</p>
                 </div> 
-                <div className="weather-country">
-                    <p>{weather.location.country}</p>
-                </div>
             </div>
             <div className="weather-condition">
                 <h2>{weather.current.condition.text}</h2>
